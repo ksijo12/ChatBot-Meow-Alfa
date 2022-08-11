bd586fb97c236eccc3e2f8b28cf86a423e18b55e
ksijo12 <sijok47@gmail.com>
2022-08-11 19:50:05 +0530

    First commit

---
+++ script/index.js
@@ -0,0 +1,14 @@
+var div = document.getElementById("chatContainer")
+function talk(){
+    var ClientText = document.getElementById("textSlot").value;
+    console.log(ClientText)
+
+    const paragraphElement = document.createElement("p").appendChild();
+    
+
+   // paragraphElement.innerHTML = ClientText;
+
+    
+   // var textPast = document.getElementById("chatContainer").appendChild(paragraphElement);
+
+}
